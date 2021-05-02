acf7b74e491d35f029732748005f7c053b257b7f
ConfusedPandaEvan <44895166+ConfusedPandaEvan@users.noreply.github.com>
2021-05-02 23:43:00 +0400

    Update app.js

--- Personal_Website/app.js
+++ Personal_Website/app.js
@@ -1,3 +1,5 @@
+//imports elements from other javascript elements
+
 import {
     Hill
 } from './hill.js';
@@ -14,13 +16,13 @@ import {
     Faucet
 } from './Faucet.js';
 
-
+//eventlistener to see if the screen is scrolled
 document.addEventListener('wheel',
         onScroll);
 function onScroll(event) {
     scroll.y = event.deltaY
     if (app){
-    
+    //If the user scrolls down a certain amount overflow is set to true and the canvas is deleted to reveal the button page
         app.fronthill.setY(scroll.y);
         if (app.fronthill.overflow){
             end();
@@ -34,7 +36,7 @@ class App{
         this.canvas = document.createElement('canvas');
         this.ctx = this.canvas.getContext("2d");
         document.body.appendChild(this.canvas);
-
+  
         this.faucet = new Faucet();
         console.log(this.faucet)
         this.backhill = new Hill('rgba(95, 92, 254, 0.7)', 0.8, 7);
@@ -44,13 +46,13 @@ class App{
         this.rubberDucks = new RubberDucks();
 
         this.mongMongs = new MongMongs();
-
+//To see when the window size is resized
         window.addEventListener('resize', this.resize.bind(this), false);
         this.resize();
 
         requestAnimationFrame(this.animate.bind(this));
     }
-
+//resize based on the screen size
     resize() {
         this.stageWidth = document.body.clientWidth;
         this.stageHeight = document.body.clientHeight;
@@ -73,7 +75,7 @@ class App{
 
     animate(t) {
         requestAnimationFrame(this.animate.bind(this));
-
+//animate all the animated elements in the canvas
         this.ctx.clearRect(0,0, this.stageWidth, this.stageHeight);
 
         this.backhill.draw(this.ctx);
@@ -87,7 +89,7 @@ class App{
         let dots;
         
         dots = this.fronthill.draw(this.ctx);
-        
+        //dots are used to calculate the y value and the tangent line of the hills
         
         this.rubberDucks.draw(this.ctx, t, dots)
         
@@ -101,7 +103,7 @@ let ended = 0;
 function end(){
     if (!ended){
         ended = 1
-    
+    //When the overflow is set to yes, it deletes the canvas to show the after screen which is a screen with buttons
     app = null;
     let html = document.getElementsByTagName("html")[0];
     let body = html.getElementsByTagName("body")[0];
@@ -112,7 +114,7 @@ function end(){
     }
 }
 window.onload = () => {
-    
+    //when the window is loaded only the canvas is shown
     app = new App();
     document.getElementById("after").style.display = 'none'
 }
