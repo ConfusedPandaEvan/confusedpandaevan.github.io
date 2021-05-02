66d1f334480711d57cbe158876b816ad594b641d
ConfusedPandaEvan <44895166+ConfusedPandaEvan@users.noreply.github.com>
2021-05-02 23:34:17 +0400

    Update MongMong.js

--- Personal_Website/MongMong.js
+++ Personal_Website/MongMong.js
@@ -2,6 +2,8 @@ import {
     Mong
 } from "./Mongs.js"
 
+//This file is to control the number of Mongs in the canvas
+
 export class MongMongs {
     constructor() {
         this.img = new Image();
@@ -20,7 +22,7 @@ export class MongMongs {
         this.stageWidth = stageWidth;
         this.stageHeight = stageHeight;
     }
-
+//This will add a Mong to the canvas when loaded
     loaded() {
         this.isLoaded = true;
         this.addMong();
@@ -31,7 +33,7 @@ export class MongMongs {
             new Mong(this.img, this.stageHeight),
         );
     }
-
+//Based on time, Mong will be added. This controls the number of Mongs on the screen
     draw(ctx, t) {
         if (this.isLoaded) {
             this.cur +=1;
@@ -39,7 +41,7 @@ export class MongMongs {
                 this.cur = 0;
                 this.addMong();
             }
-
+//If a Mong moves out of the screen, delete the Mong and Add a new Mong to the canvas
             for (let i = this.items.length - 1; i >= 0; i--) {
                 const item = this.items[i];
                 if (item.x < -item.width) {
